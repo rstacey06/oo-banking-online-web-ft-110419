206eff1127f6cc1923c4a7666d71c3d78a88333c
Burbage Stacey <rstacey06@gmail.com>
2019-11-19 20:13:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,8 +17,8 @@ class Transfer
    def execute_transaction 
      if valid? && sender.balance > amount 
        return self.status == "pending"
-       sender -= amount 
-       receiver += amount 
+       sender.balance -= amount 
+       receiver.balance += amount 
      else self.status == "rejected"
      end
        
