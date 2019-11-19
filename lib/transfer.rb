28caca85a670f386cc37f56f460ea0db173c325b
Burbage Stacey <rstacey06@gmail.com>
2019-11-19 19:42:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,5 +2,13 @@ class Transfer
   
   attr_reader :sender, :receiver, :amount 
   attr_accessor :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender 
+    @receiver = receiver
+    @amount = amount 
+    @status = status 
+  end 
+    
 end
 
